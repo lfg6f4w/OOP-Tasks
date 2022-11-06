9b5c720be3e03dea9e03bbae025c15c09b182c2e
Mohamed Abdalrazek <91884590+lfg6f4w@users.noreply.github.com>
2022-11-06 08:34:24 +0200

    Create Homwork2_5.cpp

---
+++ Homwork2_5.cpp
@@ -0,0 +1,55 @@
+#include                             <bits/stdc++.h>
+#define     ll                       long long
+#define     ld                       long double
+#define     ft                       first
+#define     sd                       second
+#define     ed                       '\n'
+#define     sz(x)                    x.size()
+#define     all(x)                   x.begin(), x.end()
+#define     rall(x)                  x.rbegin(), x.rend()
+#define     fixed(x)                 fixed<<setprecision(x)
+#define     mod                      1'000'000'007
+#define     OO                       2'000'000'000
+#define     EPS                      1e-7
+#define     pi                       acos(-1)
+#define     Good_Bay                 return
+using namespace std;
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x: v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) {
+    for (const T &x: v) out << x << ' ';
+    return out;
+}
+void Accept(){
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+class HowAddress {
+private:
+	string my;
+public:
+	HowAddress(string m) :my(m) {}
+	void PrintAddress() {
+		cout << my << ed;
+		cout << (&HowAddress::PrintAddress) << ed;
+		cout << &my << ed;
+	}
+};
+int main() {
+	Accept();
+	HowAddress name1("Dr.rabi3");
+	name1.PrintAddress();
+	HowAddress name2("Mohamed");
+	cout << ed;
+	name2.PrintAddress();
+	return 0;
+}
