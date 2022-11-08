bb5386d47f746beac79313ca536537dcc4b14104
Mohamed Abdalrazek <91884590+lfg6f4w@users.noreply.github.com>
2022-11-08 23:19:42 +0200

    Create Homwork Destructor 8.cpp

---
+++ Homwork Destructor 8.cpp
@@ -0,0 +1,56 @@
+#include                             <bits/stdc++.h>
+#define     ll                       long long
+#define     ld                       long double
+#define     ft                       first
+#define     sd                       second
+#define     ed                       '\n'
+#define     sz(x)                    x.size()
+#define     all(x)                   x.begin(), x.end()
+#define     rall(x)                  x.rbegin(), x.rend()
+#define     fixed(x)                 fixed<<setprecision(x)
+#define     mod                      1'000'000'007
+#define     OO                       2'000'000'000
+#define     EPS                      1e-7
+#define     pi                       acos(-1)
+#define     Good_Bay                 return
+using namespace std;
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x: v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) {
+    for (const T &x: v) out << x << ' ';
+    return out;
+}
+void Accept(){
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+class A {
+private:
+	int *x = nullptr;
+public:
+	A() {
+		cout<<"A constructor\n";
+		x = new int;
+		*x = 10;
+	}
+	~A() {
+		cout << "A destructor\n";
+		delete x;
+	}
+
+};
+int main() {
+	Accept();
+	A* a = new A();
+	delete a;
+	return 0;
+}
